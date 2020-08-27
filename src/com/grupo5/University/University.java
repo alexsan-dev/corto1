bd5a50160921c5413549b53b7a4c6af1897235b3
alexsan-dev <alexdanielsantosv@gmail.com>
2020-08-27 12:25:24 -0600

    [Add] class University

--- src/com/grupo5/University/University.java
+++ src/com/grupo5/University/University.java
@@ -1,7 +1,45 @@
 package com.grupo5.University;
 
 public class University {
-    public University(){
+    // ATRIBUTOS
+    public String name;
+    public String[] schools;
+    private boolean isPublic;
+    private int students;
 
+    // CONSTRUCTOR POR DEFECTO
+    public University(){
+        name = "Universidad de ...";
+        schools = new String[]{"Ingenieria", "Medicina", "Arquitectura"};
+        isPublic = true;
+        students = 10000;
+    }
+
+    // ASIGNAR VALORES INICIALES
+    public University(String name, String[] schools, boolean isPublic, int students){
+        this.name = name;
+        this.schools = schools;
+        this.isPublic = isPublic;
+        this.students = students;
+    }
+
+    // ASIGNAR NOMBRE
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // ASIGNAR ESCUELAS
+    public void setSchools(String[] schools) {
+        this.schools = schools;
+    }
+
+    // ASIGNAR SI ES PUBLICA O PRIVADA
+    public void setPublic(boolean isPublic) {
+        this.isPublic = isPublic;
+    }
+
+    // ASIGNAR ESTUDIANTES
+    public void setStudents(int students) {
+        this.students = students;
     }
 }
