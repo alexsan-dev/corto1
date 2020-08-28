091c79bffd14e1f56910163ecce6ca99cf56fe74
brandoortega630 <70341714+brandoortega630@users.noreply.github.com>
2020-08-28 00:36:06 -0600

    Update Hospital.java

--- src/com/grupo5/Hospital/Hospital.java
+++ src/com/grupo5/Hospital/Hospital.java
@@ -1,6 +1,24 @@
 package com.grupo5.Hospital;
 
 public class Hospital {
-Qw
-    public Hospital(){}
+//Atributos
+public String name;
+public int doctores;
+public int enfermeros; 
+
+//Constructor de asignaciones
+public Hospital (String name, int doctores, int enfermeros){
+    this.name=name;
+    this.doctores=doctores;
+    this.enfermeros=enfermeros;
+}
+
+//Metodos
+public void Showdoctores(int doctores){
+this.doctores=doctores;
+}
+    
+public Hospital(){
+    
+}
 }
