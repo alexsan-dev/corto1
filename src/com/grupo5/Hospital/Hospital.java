d0c56cd1cf8d8fe24efcff77b328dc327bb817b3
JavierSantos512 <70341732+JavierSantos512@users.noreply.github.com>
2020-08-28 14:45:55 -0600

    Update Hospital.java

--- src/com/grupo5/Hospital/Hospital.java
+++ src/com/grupo5/Hospital/Hospital.java
@@ -6,17 +6,20 @@ public String name;
 public int doctores;
 public int enfermeros; 
     
-public  int  paciente ;
+public  int  pacientes ;
 public  int  limpieza ;
 public  int  Seguridad ;
 
     
 
 //Constructor de asignaciones
-public Hospital (String name, int doctores, int enfermeros){
+public Hospital (String name, int doctores, int enfermeros,int pacientes,int limpeza,int seguridad){
     this.name=name;
     this.doctores=doctores;
     this.enfermeros=enfermeros;
+    this.pacientes=pacientes;
+    this.limpieza=No de empleados;
+    this.seguridad=No de policias;
 }
 
 //Metodos
