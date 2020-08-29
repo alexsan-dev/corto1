33b9562f6b74263e8d9c038773c76b31cc1ce80b
Alex Santos <alexdanielsantosv@gmail.com>
2020-08-28 22:49:49 -0600

    Create Departamento.java

---
+++ src/com/grupo5/University/Departamento.java
@@ -0,0 +1,11 @@
+public class Departamento extends University{
+  String nombre;
+  
+  public Departamento(){
+    nombre = "";
+  }
+  
+  public String printNombre(){
+    System.out.println(this.nombre);
+  }
+}
