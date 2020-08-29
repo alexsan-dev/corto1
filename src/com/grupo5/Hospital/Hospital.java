969ed9e6177806b505bc7c40e1b87b83b9cceb7c
Brandon Ortega <70341714+brandoortega630@users.noreply.github.com>
2020-08-29 00:15:10 -0600

    Update Hospital.java

--- src/com/grupo5/Hospital/Hospital.java
+++ src/com/grupo5/Hospital/Hospital.java
@@ -5,10 +5,11 @@ public class Hospital {
 public String name;
 public int doctores;
 public int enfermeros; 
-    
 public  int  pacientes ;
 public  int  limpieza ;
 public  int  Seguridad ;
+public int hospital;
+public int visitantes;
 
    // VALORES POR DEFECTO
     public Hospital (){
@@ -17,25 +18,29 @@ public  int  Seguridad ;
         seguridad = 75;
         doctores= 200;
         enfermeros= 100;
+        hospital=3;
+        visitantes=500;
     } 
 
 //Constructor de asignaciones
-public Hospital (String name, int doctores, int enfermeros,int pacientes,int limpeza,int seguridad){
+public Hospital (String name, int doctores, int enfermeros,int pacientes,int limpeza,int seguridad, int hospital, int visitantes){
     this.name=name;
     this.doctores=doctores;
     this.enfermeros=enfermeros;
     this.pacientes=pacientes;
     this.limpieza= limpieza;
     this.seguridad=seguridad;
+    this.hospital=hospital;
+    this.visitantes=visitantes;
 }
 
 //Metodos
-public void Showdoctores(int doctores){
+    public void Showdoctores(int doctores){
 this.doctores=doctores;
 }
     
-public Hospital(){
-this.enfermeros=enfermos;    
+    public Hospital(){
+this.hospital=hospital;    
 }
     public  void   pacientes () {
 System.out.println(No de pacientes + pacientes);
@@ -43,7 +48,6 @@ System.out.println(No de pacientes + pacientes);
 
     public  void   limpieza () {
 System.out.println(No de empleados de limpieza + limpieza);
-
 }
 
     public  void  seguridad () {
@@ -53,7 +57,24 @@ System.out.println(No de agentes de seguridad + seguridad);
     public void enfermeros(){
 System.out.println(No de enfermeros + enfermeros);
 }
+    
+    public void sedesHospital(){
+System.out.println(No de hospital + hospital);
+}
+    
+    public void pacientes(){
+this.pacientes=pacientes;        
+}
+    
+    public void area(){
+ area();
+}
+    
+    public void visitantes(){
+System.out.println(No. de visitantes + visitantes);        
+}
         
+    
     
 }
 
